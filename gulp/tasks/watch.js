25bacb48b383252795d4a6e741344f7fd1ada989
Andrew Leykin <katashi1328@mail.ru>
2017-08-21 11:18:17 +0300

    add hover to social

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,6 +7,6 @@ module.exports = function() {
     $.gulp.watch('./source/template/**/*.pug', $.gulp.series('pug'));
     $.gulp.watch('./source/fonts/**/*.*', $.gulp.series('copy:fonts'));
     $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy:image'));
-    $.gulp.watch('./source/sprite/*.{png,gif}', $.gulp.series('sprite:create'));
+    $.gulp.watch('./source/sprite/*.gif', $.gulp.series('sprite:create'));
   });
 };
